9bdbe887b52c0a47627a0760d0232e5f36235104
Marek Kobida <marek.kobida@gmail.com>
2023-03-31 12:57:49 +0200

    update

--- private/Client.tsx
+++ private/Client.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
+interface Product {
+  name: string;
+}
+
 function Client() {
+  const products: Product[] = [
+    {
+      name: 'Car',
+    },
+    {
+      name: 'Phone',
+    },
+  ];
+
   return (
-    <div className="message" p="2">
-      Client
+    <div alignItems="center" display="flex" flexDirection={['column', { '#': 'row' }]} justifyContent="center">
+      {products.map(product => (
+        <div key={product.name} p="4">
+          {product.name}
+        </div>
+      ))}
     </div>
   );
 }
