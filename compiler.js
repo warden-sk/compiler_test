4b4e7a7f0136aac7d1dc0cc8d739d4bfa152fe6e
Marek Kobida <marek.kobida@gmail.com>
2023-03-31 13:42:47 +0200

    update

--- compiler.js
+++ compiler.js
@@ -5,6 +5,10 @@ function compiler(code) {
   const filePath = this.resourcePath;
 
   if (/\.tsx?$/.test(filePath)) {
+    /**
+     * For more information, visit the compiler documentation at:
+     * https://github.com/warden-sk/compiler
+     */
     code = compile(filePath, {
       assets: ['./index.css', './index.js'],
       outputPath: './public',
