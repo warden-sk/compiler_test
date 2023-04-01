5122cc4f4242e0b1d2965fcf11842ea9d1e742a9
Marek Kobida <marek.kobida@gmail.com>
2023-04-01 18:39:38 +0200

    update

--- private/Client.tsx
+++ private/Client.tsx
@@ -9,17 +9,41 @@ interface Thing {
 function Client() {
   const [things, updateThings] = React.useState<Thing[]>([]);
 
-  function done(inputThing: string) {
-    updateThings(things =>
-      things.map(thing => (thing.input === inputThing ? { ...thing, isDone: !thing.isDone } : thing))
-    );
+  function onDelete(input: string) {
+    return (e: React.MouseEvent<HTMLDivElement>) => {
+      e.stopPropagation();
+
+      updateThings(things => {
+        return things.filter(thing => {
+          return thing.input !== input;
+        });
+      });
+    };
+  }
+
+  function onDone(input: string) {
+    return (e: React.MouseEvent<HTMLDivElement>) => {
+      updateThings(things => {
+        return things.map(thing => {
+          if (thing.input === input) {
+            return { ...thing, isDone: !thing.isDone };
+          } else {
+            return thing;
+          }
+        });
+      });
+    };
   }
 
   function onKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
     if (e.key === 'Enter') {
       const input: string = e.currentTarget.value;
 
-      updateThings(things => [...things, { input, isDone: false }]);
+      updateThings(things => {
+        const newThing = { input, isDone: false };
+
+        return [...things, newThing];
+      });
 
       e.currentTarget.value = '';
     }
@@ -29,8 +53,9 @@ function Client() {
     <div className="container" mX="auto">
       <div p="4" spaceY="4">
         <input
-          border="1"
+          border="2"
           borderRadius="2"
+          className="input"
           onKeyDown={onKeyDown}
           p="2"
           placeholder="What has to be done?"
@@ -39,11 +64,22 @@ function Client() {
         />
         <div spaceY="2">
           {things.map(({ input, isDone }) => (
-            <div alignItems="center" cursor="pointer" display="flex" key={input} onClick={() => done(input)} spaceX="4">
-              <div border="1" borderRadius="2" className={isDone && 'done'} fontWeight="600" lineHeight="1" p="2">
+            <div alignItems="center" cursor="pointer" display="flex" key={input} onClick={onDone(input)} spaceX="4">
+              <div
+                border="2"
+                borderRadius="2"
+                className={['done', `done_${isDone}`]}
+                fontWeight="600"
+                lineHeight="1"
+                opacity={isDone ? '75' : '25'}
+                p="2"
+              >
                 {'\u2713'}
               </div>
               <div fontWeight={isDone && '600'}>{input}</div>
+              <div fontWeight="600" lineHeight="1" mL="auto" onClick={onDelete(input)} p="2">
+                {'\u2717'}
+              </div>
             </div>
           ))}
         </div>
