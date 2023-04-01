413d8f4024e2c9ed7a510f69539a8de96f2279a7
Marek Kobida <marek.kobida@gmail.com>
2023-04-01 14:26:28 +0200

    update

--- private/Client.tsx
+++ private/Client.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
+import './Client.css';
 
-interface Product {
-  name: string;
+interface Thing {
+  input: string;
+  isDone: boolean;
 }
 
 function Client() {
-  const products: Product[] = [
-    {
-      name: 'Car',
-    },
-    {
-      name: 'Phone',
-    },
-  ];
+  const [things, updateThings] = React.useState<Thing[]>([]);
+
+  function done(inputThing: string) {
+    updateThings(things =>
+      things.map(thing => (thing.input === inputThing ? { ...thing, isDone: !thing.isDone } : thing))
+    );
+  }
+
+  function onKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key === 'Enter') {
+      const thing: string = e.currentTarget.value;
+
+      updateThings(things => [...things, { input: thing, isDone: false }]);
+
+      e.currentTarget.value = '';
+    }
+  }
 
   return (
-    <div alignItems="center" display="flex" flexDirection={['column', { '#': 'row' }]} justifyContent="center">
-      {products.map(product => (
-        <div key={product.name} p="4">
-          {product.name}
-        </div>
-      ))}
+    <div p="4" spaceY="4">
+      <input border="1" onKeyDown={onKeyDown} p="2" type="text" width="100" />
+      <div spaceY="2">
+        {things.map(({ input, isDone }) => (
+          <div alignItems="center" cursor="pointer" display="flex" key={input} onClick={() => done(input)} spaceX="4">
+            <div border="1" borderRadius="50" className={isDone && 'done'} p="4" />
+            <div>{input}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
