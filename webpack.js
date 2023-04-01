266f59acbc9bcd7743f653bd10ab1414641fc72b
Marek Kobida <marek.kobida@gmail.com>
2023-04-01 13:29:50 +0200

    fixed

--- webpack.js
+++ webpack.js
@@ -2,11 +2,8 @@
 const path = require('path');
 const webpack = require('webpack');
 
-const inputPath = path.resolve(__dirname, './private');
-const outputPath = path.resolve(__dirname, './public');
-
 const compiler = webpack({
-  entry: path.resolve(inputPath, './index.tsx'),
+  entry: path.resolve('./private/index.tsx'),
   mode: 'development',
   module: {
     rules: [
@@ -20,7 +17,7 @@ const compiler = webpack({
     filename: 'index.js',
     globalObject: 'this',
     libraryTarget: 'umd2',
-    path: outputPath,
+    path: path.resolve(__dirname, './public'),
   },
   resolve: {
     extensions: ['.js', '.json', '.ts', '.tsx'],
