ef4e5c249a65b30ea4abe47e957abac1e1ad2646
Marek Kobida <marek.kobida@gmail.com>
2023-04-02 12:27:55 +0200

    update

--- compiler.js
+++ compiler.js
@@ -4,6 +4,10 @@ const compile = require('@warden-sk/compiler').default;
 function compiler(code) {
   const filePath = this.resourcePath;
 
+  if (/\.css$/.test(filePath)) {
+    return;
+  }
+
   if (/\.tsx?$/.test(filePath)) {
     code = compile(filePath, {
       assets: ['./index.css', './index.js'],
