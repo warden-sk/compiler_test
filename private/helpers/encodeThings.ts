35fa0dd7ec60ac6c0caa5c6d7d241d568052feb9
Marek Kobida <marek.kobida@gmail.com>
2023-04-05 00:12:46 +0200

    update

--- private/helpers/encodeThings.ts
+++ private/helpers/encodeThings.ts
@@ -9,10 +9,13 @@ import type { ThingType } from '../Client';
  * To "0,0,Thing 1;0,1,Thing 2"
  */
 function encodeThings(things: ThingType[]): string {
-  return things.reduce(
-    ($, thing, i) => `${i ? `${$};` : ''}${+thing.createdAt},${thing.isDone ? 1 : 0},${thing.key}`,
-    ''
-  );
+  return things.reduce(($, thing, i) => {
+    const createdAt: number = +thing.createdAt;
+    const isDone: number = thing.isDone ? 1 : 0;
+    const key: string = thing.key.replace(/([:;])/g, '\\$1');
+
+    return `${i ? `${$};` : ''}${createdAt},${isDone},${key}`;
+  }, '');
 }
 
 export default encodeThings;
