6b278f229405af113704d3d707a4ab26b36b784e
Marek Kobida <marek.kobida@gmail.com>
2023-04-04 12:39:23 +0200

    added comments

--- private/helpers/decodeThings.ts
+++ private/helpers/decodeThings.ts
@@ -2,6 +2,14 @@ import type { Thing } from '../Client';
 
 const pattern = /(false|true),([^;]+)/g;
 
+/**
+ * From "false,Thing 1;true,Thing 2"
+ *
+ * To [
+ *   { isDone: false, key: 'Thing 1' },
+ *   { isDone: true, key: 'Thing 2' },
+ * ]
+ */
 function decodeThings(things: string): Thing[] {
   let $;
   let decodedThings: Thing[] = [];
--- private/helpers/encodeThings.ts
+++ private/helpers/encodeThings.ts
@@ -1,5 +1,13 @@
 import type { Thing } from '../Client';
 
+/**
+ * From [
+ *   { isDone: false, key: 'Thing 1' },
+ *   { isDone: true, key: 'Thing 2' },
+ * ]
+ *
+ * To "false,Thing 1;true,Thing 2"
+ */
 function encodeThings(things: Thing[]): string {
   return things.reduce(($, thing, i) => {
     const text = `${thing.isDone},${thing.key}`;
