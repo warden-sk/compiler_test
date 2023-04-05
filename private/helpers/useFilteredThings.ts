b74dc1a106bb0a3ec830505b228d2dffd13427c1
Marek Kobida <marek.kobida@gmail.com>
2023-04-05 14:29:15 +0200

    useFilteredThings

---
+++ private/helpers/useFilteredThings.ts
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2023 Marek Kobida
+ */
+
+import React from 'react';
+import context from '../helpers/context';
+import type { Thing } from '../types';
+
+function useFilteredThings(list?: string): Thing[] {
+  const { currentList, things } = React.useContext(context);
+
+  const $: string = list ?? currentList;
+
+  let filteredThings = [...things];
+
+  if ($ === 'Done') {
+    filteredThings = filteredThings.filter(thing => thing.isDone);
+  }
+
+  if ($ === 'Not done') {
+    filteredThings = filteredThings.filter(thing => !thing.isDone);
+  }
+
+  return filteredThings;
+}
+
+export default useFilteredThings;
