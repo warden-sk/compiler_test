7ca68931ea91404106c063810abce58badf21302
Marek Kobida <marek.kobida@gmail.com>
2023-04-19 17:42:49 +0930

    update

--- private/components/Client/index.tsx
+++ private/components/Client/index.tsx
@@ -6,17 +6,15 @@ import React from 'react';
 import context from '../../helpers/context';
 import decodeThings from '../../helpers/decodeThings';
 import encodeThings from '../../helpers/encodeThings';
-import Calendar from '../../test/Calendar';
-import EnhancedDate from '../../test/helpers/EnhancedDate';
 import type { Thing } from '../../types';
+import Input from '../Input';
+import Lists from '../Lists';
+import Things from '../Things';
 import './index.css';
 
 function Client() {
   const [currentList, setCurrentList] = React.useState<string>('All');
   const [things, setThings] = React.useState<Thing[]>([]);
-
-  const date1st = React.useState<EnhancedDate>(new EnhancedDate().date());
-  const date2nd = React.useState<EnhancedDate>(new EnhancedDate(2024, 11, 31).date());
 
   React.useEffect(() => {
     const things = localStorage.getItem('things');
@@ -34,21 +32,12 @@ function Client() {
     <context.Provider value={{ currentList, setCurrentList, setThings, things }}>
       <div className="container" height="100" mX="auto" p="4">
         <div display="flex" flexDirection="column" height="100" spaceY="4">
-          <div display="flex" fontSize="2" justifyContent="center" spaceX="4">
-            <div>od {date1st[0].toLocaleDateString()}</div>
-            {+date2nd[0] === +new EnhancedDate(2024, 11, 31).date() ? (
-              <div>{'\u2014'}</div>
-            ) : (
-              <div>do {date2nd[0].toLocaleDateString()}</div>
-            )}
+          <Lists />
+          <Input />
+          <div fontSize="8" fontWeight="600">
+            Things
           </div>
-          <Calendar count={2} date1st={date1st} date2nd={date2nd} />
-          {/*<Lists />*/}
-          {/*<Input />*/}
-          {/*<div fontSize="8" fontWeight="600">*/}
-          {/*  Things*/}
-          {/*</div>*/}
-          {/*<Things />*/}
+          <Things />
           <div fontSize="2" opacity="50" textAlign="center">
             Copyright 2023 Marek Kobida
           </div>
--- private/test/helpers/createArrayOf.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2023 Marek Kobida
- */
-
-function createArrayOf(i: number): number[] {
-  return [...new Array(i)].map((...[, j]) => j + 1);
-}
-
-export default createArrayOf;
