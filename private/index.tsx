4df3e4292703de81208f52d6c7c001110be28299
Marek Kobida <marek.kobida@gmail.com>
2023-04-20 20:45:51 +0930

    update

--- private/index.tsx
+++ private/index.tsx
@@ -8,7 +8,7 @@ import Client from './components/Client';
 import './index.css';
 
 if (typeof window !== 'undefined') {
-  ReactDOM.createRoot(window.document.querySelector('#client')!).render(<Client />);
+  ReactDOM.createRoot(window.document.querySelector('#client') as HTMLDivElement).render(<Client />);
 }
 
 export default <div height="100" id="client" />;
