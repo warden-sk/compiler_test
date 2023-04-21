f9b3884cf34cc4fe981045e1a146d316cac2c4fc
Marek Kobida <marek.kobida@gmail.com>
2023-04-22 07:38:01 +0930

    update

--- private/helpers/encodeThings.ts
+++ private/helpers/encodeThings.ts
@@ -9,8 +9,8 @@ function encodeThings(things: Thing[]): string {
     const createdAt: number = +thing.createdAt;
     const doneAt: number = thing.doneAt ? +thing.doneAt : -1;
     const isDone: number = thing.isDone ? 1 : 0;
-    const key: string = thing.key.replace(/([:;])/g, '\\$1');
-    const list: number | string = thing.list ? thing.list.replace(/([:;])/g, '\\$1') : -1;
+    const key: string = thing.key;
+    const list: string = thing.list ? thing.list : '-1';
 
     return `${i ? `${$};` : ''}${createdAt},${doneAt},${isDone},${key},${list}`;
   }, '');
